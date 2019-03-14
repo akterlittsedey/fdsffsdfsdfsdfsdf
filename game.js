cefa9da62cca949854e9059d006aff9fe26d3ca3
akterlittsedey <46784071+akterlittsedey@users.noreply.github.com>
2019-03-14 16:52:12 +0300

    fff
    game

--- game.js
+++ game.js
@@ -42,7 +42,146 @@ var game = {
 	speedTextValue = game.add.text(558,18,speed.tostring(),textstyle_Value);
 	},
 	
-		
+	update: function ()
+  if (cursors.right.isDown && direction !: 'left')
+  { 
+    New_direction = 'right';
+  }
+  else if (cursors.left.isDown && direction != 'right')
+        {
+            new_direction = 'left';
+        }
+        else if (cursors.up.isDown && direction != 'down')
+        {
+            new_direction = 'up';
+        }
+        else if (cursors.down.isDown && direction != 'up')
+        {
+            new_direction = 'down';
+        }
+
+        
+        speed = Math.min(10, Math.floor(score / 5));
+         
+        speedTextValue.text = '' + speed;
+
+          
+        updateDelay++;
+
+        
+        if (updateDelay % (10 - speed) == 0)
+        {
+             
+
+            var firstCell = snake[snake.length - 1],
+                lastCell = snake.shift(),
+                oldLastCellx = lastCell.x,
+                oldLastCelly = lastCell.y;
+
+            
+            if (new_direction)
+            {
+                direction = new_direction;
+                new_direction = null;
+            }
+
+            
+            if (direction == 'right')
+            {
+                lastCell.x = firstCell.x + 15;
+                lastCell.y = firstCell.y;
+            }
+            else if (direction == 'left')
+            {
+                lastCell.x = firstCell.x - 15;
+                lastCell.y = firstCell.y;
+            }
+            else if (direction == 'up')
+            {
+                lastCell.x = firstCell.x;
+                lastCell.y = firstCell.y - 15;
+            }
+            else if (direction == 'down')
+            {
+                lastCell.x = firstCell.x;
+                lastCell.y = firstCell.y + 15;
+            }
+
+            
+            snake.push(lastCell);
+            firstCell = lastCell;
+
+              
+            if (addNew)
+            {
+                snake.unshift(game.add.sprite(oldLastCellx, oldLastCelly, 'snake'));
+                addNew = false;
+            }
+
+             
+            this.appleCollision(); 
+             
+            this.selfCollision(firstCell); 
+             
+            this.wallCollision(firstCell);
+        }
+    },
+
+    generateApple: function ()
+    {
+         
+        var randomX = Math.floor(Math.random() * 40) * squareSize, 
+            randomY = Math.floor(Math.random() * 30) * squareSize; 
+        
+        apple = game.add.sprite(randomX, randomY, 'apple'); 
+    },
+
+    appleCollision: function ()
+    {
+          
+        for (var i = 0; i < snake.length; i++) 
+        { 
+            if (snake[i].x == apple.x && snake[i].y == apple.y) 
+            { 
+                 
+                addNew = true; 
+                 
+                apple.destroy(); 
+                 
+                this.generateApple(); 
+                 
+                score++; 
+                 
+                scoreTextValue.text = score.toString(); 
+            }
+        }
+
+    },
+
+    selfCollision: function (head) 
+    { 
+        
+        for (var i = 0; i < snake.length - 1; i++) 
+        { 
+            if (head.x == snake[i].x && head.y == snake[i].y) 
+            { 
+                  
+                game.state.start('Game_Over'); 
+            } 
+        } 
+    }, 
+    wallCollision: function (head) 
+    { 
+         
+        if (head.x >= 600 || head.x < 0 || head.y >= 450 || head.y < 0) 
+        { 
+            
+            game.state.start('Game_Over'); 
+        } 
+    }
+
+};
+	
 	
 	
 	
